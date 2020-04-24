074fd8257ed20fffbf8135f7570ec772ba150d8d
root <root@f83f9220f91c>
2020-04-24 22:07:41 +0000

    the riddler

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,22 @@
 
 void setup() {
-    
+  size(600,600);
+      fill(216, 216, 22);
+  ellipse(300, 300, 300, 300);
+  fill(206, 4, 11);
+  ellipse(300,300, 250, 250);
+  fill(245, 250 , 3);
+  ellipse(300, 300, 200, 200);
 }
 void draw() {
-    
+  PImage corn = loadImage("corn.jpg");
+  corn.resize(50, 50);
+  image(corn, 300, 300);
+  PImage peperoni = loadImage("peperoni.jpeg");
+  peperoni.resize(50, 50);
+  image(peperoni, 250, 250);
+  if(mousePressed){
+ image(peperoni, mouseX, mouseY);
+ image(corn, mouseX +100, mouseY);
+}
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,26 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String riddle1 = JOptionPane.showInputDialog("i am greater then good, " + 
+		"more evil then evil, " + 
+		"the poor have me, " + 
+		"the rich need me, " + 
+		"if you eat me, you will die, " + 
+		"who am i.");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if(riddle1.equals ("nothing")) {
+JOptionPane.showMessageDialog(null, "corect!");
+score += 10;}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+	JOptionPane.showMessageDialog(null, "wrong! the corect answer is nothing");}
+}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
 		
-	}
+	
 }
 
